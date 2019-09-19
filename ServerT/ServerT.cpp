a97be7e9eb4406285cacc02a9dd4bfb444160115
Yuliya Maximchikova <m.yuliyas.00@gmail.com>
2019-09-19 23:19:06 +0300

    Добавьте файлы проекта.

---
+++ ServerT/ServerT.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include "Winsock2.h"
+
+#pragma comment(lib, "WS2_32.lib")  
+
+using namespace std;
+
+string GetErrorMsgText(int code)									// cформировать текст ошибки 
+{
+	string msgText;
+	switch (code)													// проверка кода возврата 
+	{
+	case WSAEINTR:          msgText = "WSAEINTR";         break;
+	case WSAEACCES:         msgText = "WSAEACCES";        break;
+	case WSASYSCALLFAILURE: msgText = "WSASYSCALLFAILURE"; break;
+	default:                msgText = "***ERROR***";      break;
+	};
+	return msgText;
+};
+
+string  SetErrorMsgText(string msgText, int code)
+{
+	return  msgText + GetErrorMsgText(code);
+};
+
+
+int main()
+{
+	try
+	{
+		WSAStartup()
+		SOCKET sS;
+		if ((sS = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET)
+			throw  SetErrorMsgText("socket:", WSAGetLastError());
+	}
+	catch (string errorMsgText)
+	{
+		cout << endl << "WSAGetLastError: " << errorMsgText;
+	}
+
+	return 0;
+}
+
