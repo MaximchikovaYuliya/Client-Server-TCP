e8a45d4d50afc7b41ba5344ec7ad95b1fe4382b9
Yuliya Maximchikova <m.yuliyas.00@gmail.com>
2019-09-23 23:28:23 +0300

    should work on listen part

--- ServerT/ServerT.cpp
+++ ServerT/ServerT.cpp
@@ -30,15 +30,38 @@ int main()
 	SOCKET sS;
 	try
 	{
-		if (FAILED(WSAStartup(MAKEWORD(2, 2), &ws)))
-			throw SetErrorMsgText("wsastartup:", WSAGetLastError());
-		if ((sS = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET)
-			throw  SetErrorMsgText("socket:", WSAGetLastError());
-		if (closesocket(sS) == SOCKET_ERROR)
-			throw  SetErrorMsgText("closesocket:", WSAGetLastError());
-		if (WSACleanup() == SOCKET_ERROR)
-			throw  SetErrorMsgText("Cleanup:", WSAGetLastError());
+		if (FAILED(WSAStartup(MAKEWORD(2, 0), &ws)))
+			throw SetErrorMsgText("STARTUP: ", WSAGetLastError());
 
+		if ((sS = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET)
+			throw  SetErrorMsgText("SOCKET: ", WSAGetLastError());
+		cout << "Socket created." << endl;
+
+		SOCKADDR_IN serv;                     // параметры  сокета sS
+		serv.sin_family = AF_INET;           // используется IP-адресация  
+		serv.sin_port = htons(2000);          // порт 2000
+		serv.sin_addr.s_addr = INADDR_ANY;
+
+		if (bind(sS, (LPSOCKADDR)& serv, sizeof(serv)) == SOCKET_ERROR)
+			throw  SetErrorMsgText("BIND: ", WSAGetLastError());
+
+		if (listen(sS, SOMAXCONN) == SOCKET_ERROR)
+			throw  SetErrorMsgText("LISTEN: ", WSAGetLastError());
+
+		SOCKET cS;	                 // сокет для обмена данными с клиентом 
+		SOCKADDR_IN clnt;             // параметры  сокета клиента
+		memset(&clnt, 0, sizeof(clnt)); // обнулить память
+		int lclnt = sizeof(clnt);    // размер SOCKADDR_IN
+
+		if ((cS = accept(sS, (sockaddr*)& clnt, &lclnt)) == INVALID_SOCKET)
+			throw  SetErrorMsgText("ACCEPT: ", WSAGetLastError());
+
+
+		if (closesocket(sS) == SOCKET_ERROR)
+			throw  SetErrorMsgText("CLOSE_SOCKET: ", WSAGetLastError());
+
+		if (WSACleanup() == SOCKET_ERROR)
+			throw  SetErrorMsgText("CLENUP: ", WSAGetLastError());
 	}
 	catch (string errorMsgText)
 	{
